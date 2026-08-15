7f255a6df2db63fbd55f450922b6856b5ee7efb4
jhonatan <lucioferreirademelojhonatan@gmail.com>
2026-08-15 19:42:27 -0400

    Função criar notas pronta e estilização dos modais

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 
 const DivWidgets = document.getElementById("widget-visualizações");
 const body = document.body;
-const OpenSidebar = document.getElementById("OpenSidebar");
+const OpenSidebar = document.getElementById("OpenSidebar")
+const TemplateNota = document.getElementById("TemplateNota");
+const ModalNota = document.getElementById("ModalNota");
+const Notas = document.getElementById("notas");
+const InputTitulo = document.getElementById("input-titulo");
+const InputNota = document.getElementById("input-nota");
+const FormNota = document.getElementById("FormNota");
 
 // funções
 
 function EhMobile() {
     window.innerWidth < 769;
+    return;
 }
 
 function relogio() {
@@ -26,6 +33,41 @@ function data(agora) {
     document.getElementById("data").textContent = `${dia}/${mes}/${ano}`;
 }
 
+function NovaNota() {
+    const valor = InputNota.value.trim();
+    const valortitulo = InputTitulo.value.trim();
+
+    const clone = TemplateNota.content.cloneNode(true);
+
+    const containerNota = clone.querySelector(".container-notas");
+    const titulo = clone.querySelector("h2");
+    const notaTexto = clone.querySelector(".nota-texto");
+    const btnMais = clone.querySelector(".btn-mais");
+    const btnEditar = clone.querySelector(".btn-editar");
+    const btnExcluir = clone.querySelector(".btn-excluir");
+
+    titulo.textContent = valortitulo || "Sem Título";
+    notaTexto.textContent = valor;
+
+    btnMais.addEventListener("click", () => {
+        notaTexto.classList.toggle("expandida");
+        btnMais.textContent = notaTexto.classList.contains("expandida") ? "Menos ⬆︎" : "Mais ⬇︎";
+    });
+
+    btnExcluir.addEventListener("click", () => {
+        containerNota.remove();
+    });
+
+    btnEditar.addEventListener("click", () => {
+        InputTitulo.value = titulo.textContent === "Sem Título" ? "" : titulo.textContent;
+        InputNota.value = notaTexto.textContent;
+        containerNota.remove();
+        ModalNota.showModal();
+    });
+
+    Notas.appendChild(clone);
+}
+
 // rodar funções
 
 EhMobile();
@@ -35,6 +77,22 @@ setInterval(relogio, 1000);
 
 data();
 setInterval(data, 1000);
+
+FormNota.addEventListener("submit", () => {
+    NovaNota();
+    ModalNota.close();
+    FormNota.reset();
+    DivWidgets.dataset.widget = "nota"
+})
+
+InputNota.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && !e.ctrlKey && !e.metaKey && !e.shiftKey) {
+        e.preventDefault();
+        NovaNota();
+        ModalNota.close();
+        DivWidgets.dataset.widget = "nota"
+    }
+});
 
 // código
 
